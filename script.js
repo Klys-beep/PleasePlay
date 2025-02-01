2c879a72bfaa9e5f52d8ff7cdcf2196a9fb45df6
Klys-beep <kzimnicki10@gmail.com>
2025-02-01 16:20:42 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("yesBtn").addEventListener("click", function() {
+    alert("Yay! ❤️");
+});
+
+document.getElementById("noBtn").addEventListener("mouseover", function() {
+    let button = document.getElementById("noBtn");
+    let randomX = Math.floor(Math.random() * 300) - 150;
+    let randomY = Math.floor(Math.random() * 300) - 150;
+    button.style.transform = `translate(${randomX}px, ${randomY}px)`;
+});
